83cc00f34fac36adc0ce612711ab236fcf3d4eb0
Alyanna Lingat <ialyanna99941@gmail.com>
2023-05-22 16:40:37 -1100

    Sort List

--- src/Employee.js
+++ src/Employee.js
@@ -5,27 +5,32 @@ import './styles/employee.css';
 const Employee = () => {
 
   const data = [
-         { id: 1, name: 'Alyanna Ella Lingat'},
-         { id: 2, name: 'Jhonie Ann Pancho'},
-         { id: 3, name: 'Shaula Khei Gentozala'},
-         { id: 4, name: 'Denmarc Pascual'},
-         { id: 5, name: 'Katherine Vyne Perez'},
-         { id: 6, name: 'Jan Princess Alconcer'},
-         { id: 7, name: 'Aejay Pido'},
-         { id: 8, name: 'Carlos Lim'},
-         { id: 9, name: 'Raphael Antonio'},
-         { id: 10, name: ' '},
-         { id: 11, name: ' '},
-         { id: 12, name: ' '},
-         { id: 13, name: ' '},
-         { id: 14, name: ' '},
-         { id: 15, name: ' '},
-         { id: 16, name: ' '},
-         { id: 17, name: ' '},
-         { id: 18, name: ' '},
-         { id: 19, name: ' '},
-         { id: 20, name: ' '},
+         { id: 2, name: 'Alyanna Ella Lingat'},
+         { id: 10, name: 'Jhonie Ann Pancho'},
+         { id: 20, name: 'Shaula Khei Gentozala'},
+         { id: 7, name: 'Denmarc Pascual'},
+         { id: 12, name: 'Katherine Vyne Perez'},
+         { id: 9, name: 'Jan Princess Alconcer'},
+         { id: 1, name: 'Aejay Pido'},
+         { id: 5, name: 'Carlos Lim'},
+         { id: 16, name: 'Raphael Antonio'},
+         { id: 15, name: 'Meljohn Chavez'},
+         { id: 13, name: 'Lance Quising'},
+         { id: 17, name: 'Rodelio Sedanza'},
+         { id: 11, name: 'Kalix Jace Martinez'},
+         { id: 14, name: 'Luna Valeria'},
+         { id: 19, name: 'Sevi Camero'},
+         { id: 8, name: 'Elyse Ledezma'},
+         { id: 6, name: 'Cylden Ramirez'},
+         { id: 18, name: 'Samantha Vera '},
+         { id: 4, name: 'Avianna Diaz'},
+         { id: 3, name: 'Ashianna Kim Fernansez'},
        ];
+
+       data.sort((a, b) => (a.name > b.name ? 1 : -1));
+       console.log(data); 
+
+     
 
   return (
     <>
